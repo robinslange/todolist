543bea26eb63702807ea8bd12225dd58887fbe24
robinslange <36828109+robinslange@users.noreply.github.com>
2020-06-13 21:07:48 +1200

    removed unnecessary node module

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-import { sync } from "vuex-router-sync";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "@fortawesome/fontawesome-free/css/all.css"; // Ensure you are using css-loader
 
 Vue.config.productionTip = false;
-
-sync(store, router);
 
 new Vue({
   router,
