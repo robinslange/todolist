00acd63880a726d6905cead22fc07b5c0b9736eb
Robin <36828109+robinslange@users.noreply.github.com>
2020-06-28 23:11:19 +1200

    added due date picker component and basic logic

---
+++ src/components/DueDatePicker.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-dialog v-model="this.$store.state.dueDateDialog">
+      <v-card>
+        <v-date-picker
+          v-model="dueDate"
+          min="today"
+          :color="this.$store.state.titleColor"
+          @click:date="setDueDate"
+        ></v-date-picker>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DueDatePicker",
+  data: () => ({}),
+  methods: {
+    setDueDate(date) {
+      console.log(date);
+    },
+  },
+  computed: {
+    today() {
+      var today = new Date();
+      var dd = String(today.getDate()).padStart(2, "0");
+      var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+      var yyyy = today.getFullYear();
+
+      today = yyyy + "/" + mm + "/" + dd;
+      return today;
+    },
+  },
+};
+</script>
+
+<style></style>
