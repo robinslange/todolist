159af93c59e743b24f07566c76548740a35783ae
robinslange <36828109+robinslange@users.noreply.github.com>
2022-05-31 15:44:25 +1200

    Update init.js
    removed vars as repo public

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -5,14 +5,14 @@ import "firebase/firestore";
 
 // firebase config
 var config = {
-  apiKey: "AIzaSyDB8vnIw7pEXPonBa0Ni9Qvkf5vOMYUVP4",
-  authDomain: "todolist-2558d.firebaseapp.com",
-  databaseURL: "https://todolist-2558d.firebaseio.com",
-  projectId: "todolist-2558d",
-  storageBucket: "todolist-2558d.appspot.com",
-  messagingSenderId: "19040480521",
-  appId: "1:19040480521:web:647a7c7913c5ee369477d8",
-  measurementId: "G-6DVRE46G75",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 //firebase.analytics();
